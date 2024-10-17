1c494f1b6b09c8396896a6824fe07ad6359af8c5
unknown <fpercep889@g.educaand.es>
2024-10-17 20:50:14 +0200

    modificar tarea 2

--- proyectos_programacion/tarea2/src/com/francisco/tarea2/Actividad03.java
+++ proyectos_programacion/tarea2/src/com/francisco/tarea2/Actividad03.java
@@ -5,28 +5,27 @@ import java.util.Scanner;
 public class Actividad03 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		double num = 0;
-		double mayor = Integer.MIN_VALUE;;
-		double menor = Integer.MAX_VALUE;;
-		double suma = 0;
+		int num = 0;
+		int mayor = Integer.MIN_VALUE;;
+		int menor = Integer.MAX_VALUE;;
+		int suma = 0;
 		int cantidad = 0;
 		
-		System.out.println("Introduce 0 para terminar");
+		System.out.print("Introduce 0 para terminar: ");
 		
 		do {
-			System.out.println("Introduce un numero numero");
-			num = scan.nextDouble();
-			if(num >= 0){
+			System.out.print("\nIntroduce un numero numero: ");
+			num = scan.nextInt();
+			if(num > 0){
 				if(num >= mayor) { mayor = num;}
 				if(num <= menor && num != 0) { menor = num;}
 				suma = num + suma;
-				cantidad ++;
-			} else {System.err.println("SOLO SE PUEDE INTRODUCIR NÚMEROS POSITIVOS");}
+				cantidad ++;}
+			else if (num != 0) {System.err.println("SOLO SE PERMITE INTRODUCIR NÚMEROS POSITIVOS");}	
 		} while (num != 0);
 		
 		if (suma != 0) {
-			cantidad --;
-			double media = suma / cantidad;
+			float media =  (float)suma / cantidad;
 			System.out.println("Has escrito " + cantidad + " números");
 			System.out.println("El mayor número es: " + mayor);
 			System.out.println("El menor número es: " + menor);
