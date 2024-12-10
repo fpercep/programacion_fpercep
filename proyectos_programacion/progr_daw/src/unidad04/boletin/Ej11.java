f0dccb0c639e983a39d4e2d7953911420ddd109b
unknown <fpercep889@g.educaand.es>
2024-12-10 15:53:28 +0100

    ajedrez

--- proyectos_programacion/progr_daw/src/unidad04/boletin/Ej11.java
+++ proyectos_programacion/progr_daw/src/unidad04/boletin/Ej11.java
@@ -3,19 +3,22 @@ package unidad04.boletin;
 public class Ej11 {
 
 	public static void main(String[] args) {
-		int[][] arrayBidimensional = new int[4][5];
+		int filas = 4;
+		int columnas = 5;
+		
+		int[][] arrayBidimensional = new int[filas][columnas];
 		arrayBidimensional[0][0] = 5;
 
 		for (int i = 0; i < arrayBidimensional.length; i++) {
 			for (int j = 0; j < arrayBidimensional[0].length; j++) {
-				int numAleatorio = (int) (Math.random() * 101);
+				int numAleatorio = (int) (Math.random() * 10);
 				arrayBidimensional[i][j] = numAleatorio;
 			}
-		}
+		}    
 		
 		int sumaFilas = 0;
 		int sumaTotal = 0;
-		int [] sumaCol = new int [5];
+		int [] sumaCol = new int [columnas];
 		for (int i = 0; i < arrayBidimensional.length; i++) {
 			for (int j = 0; j < arrayBidimensional[0].length; j++) {
 				System.out.print(arrayBidimensional[i][j] + "\t");
